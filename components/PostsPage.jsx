5477020926c32bbbc9cc57c1afa5963ae1dfc9c1
Ana <anasau28@gmail.com>
2021-03-20 10:21:32 +0000

    refactor:PostsPage

--- components/PostsPage.jsx
+++ components/PostsPage.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-
 import PostCard from '@/PostCard'
 import Sidepanel from '@/Sidepanel'
-
 import fetchPosts from '../api/fetchPosts'
 
 const PostsPage = () => {
   const [posts, setPosts] = React.useState([])
 
-  React.useEffect(async () => {
-    const posts = await fetchPosts()
-    setPosts(posts)
+  React.useEffect(() => {
+    async function fetch() {
+      try {
+        const posts = await fetchPosts()
+        setPosts(posts)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fetch()
   }, [])
 
   const [sidepanelContent, setSidepanelContent] = React.useState(null)
@@ -19,7 +24,7 @@ const PostsPage = () => {
     <>
       <div className="grid grid-cols-12 gap-4">
         {posts.map((post) => {
-          return <PostCard post={post} />
+          return <PostCard post={post} key={post.id} />
         })}
       </div>
       <Sidepanel content={sidepanelContent} setContent={setSidepanelContent} />
